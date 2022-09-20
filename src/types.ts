162391f9f3d3adcde84e7d86cb583ddb4902eaf9
UpperCod <uppercod.git@gmail.com>
2022-09-20 15:02:14 -0300

    add GetProps

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,6 @@ export interface CustomElementStatic extends CustomElementConstructor {
     "##props": any;
     props: any;
 }
+
+export type GetProps<CustomElement extends CustomElementStatic> =
+    CustomElement extends { "##props": infer P } ? P : any;
